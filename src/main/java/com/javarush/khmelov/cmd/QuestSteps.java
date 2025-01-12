0e44bc0e8180d566034a4dec7bf0b141f5e71e98
shishkin <Craz9.Bunn9@yandex.ru>
2025-01-12 10:54:54 +0300

    fix: statistics added to logic

--- src/main/java/com/javarush/khmelov/cmd/QuestSteps.java
+++ src/main/java/com/javarush/khmelov/cmd/QuestSteps.java
@@ -1,29 +1,37 @@
 package com.javarush.khmelov.cmd;
 
 import com.javarush.khmelov.entity.Quest;
+import com.javarush.khmelov.entity.Statistic;
+import com.javarush.khmelov.entity.User;
 import com.javarush.khmelov.service.QuestService;
+import com.javarush.khmelov.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @SuppressWarnings("unused")
 public class QuestSteps implements Command {
     private final QuestService questService;
+    private final UserService userService;
 
-    public QuestSteps(QuestService questService) {
+    public QuestSteps(QuestService questService, UserService userService) {
         this.questService = questService;
+        this.userService = userService;
     }
 
     @Override
     public String doGet(HttpServletRequest request) {
         String stringId = request.getParameter("id");
 
-        if(stringId.equals("5")){
-            stringId = getVictory(request);
+        if (stringId.equals("4")) {
+            setVictory(request);
         }
-
-        if(request.getParameter("name")!=null) {
+        if (stringId.equals("5")) {
+            stringId = "1";
+        }
+        if (request.getParameter("name") != null) {
             setQuestName(request);
         }
 
@@ -32,21 +40,14 @@ public class QuestSteps implements Command {
             Optional<Quest> optionalQuest = questService.get(id);
             if (optionalQuest.isPresent()) {
                 Quest quest = optionalQuest.get();
-                if(id == 0){
+                if (id == 0) {
                     quest.setQuestion(request.getSession().getAttribute("questName") + getReason(request));
                 }
                 request.setAttribute("quest", quest);
             }
         }
-        if(stringId.equals("1")){
-            HttpSession session = request.getSession();
-            Integer totalCount = (Integer) session.getAttribute("totalCount");
-            if (totalCount == null) {
-                totalCount = 1;
-                session.setAttribute("totalCount", totalCount);
-            } else {
-                session.setAttribute("totalCount", ++totalCount);
-            }
+        if (stringId.equals("1")) {
+            setStartQuest(request);
         }
         return getView();
     }
@@ -55,7 +56,7 @@ public class QuestSteps implements Command {
     public String doPost(HttpServletRequest request) {
         String stringId = request.getParameter("id");
 
-        if(request.getParameter("name")!=null) {
+        if (request.getParameter("name") != null) {
             setQuestName(request);
         }
         if (stringId != null) {
@@ -66,34 +67,66 @@ public class QuestSteps implements Command {
                 request.setAttribute("quest", quest);
             }
         }
-        if(stringId.equals("1")){
-            HttpSession session = request.getSession();
-            Integer totalCount = (Integer) session.getAttribute("totalCount");
-            if (totalCount == null) {
-                totalCount = 1;
-                session.setAttribute("totalCount", totalCount);
-            } else {
-                session.setAttribute("totalCount", ++totalCount);
-            }
+        if (stringId.equals("1")) {
+            setStartQuest(request);
         }
         return getView();
     }
 
-    private String getVictory(HttpServletRequest request) {
+    private void setStartQuest(HttpServletRequest request) {
         HttpSession session = request.getSession();
-        Integer CountVictorys = (Integer) session.getAttribute("CountVictory");
-        if (CountVictorys == null) {
-            CountVictorys = 1;
-            session.setAttribute("CountVictory", CountVictorys);
-        } else {
-            session.setAttribute("CountVictory", ++CountVictorys);
+        if (session.getAttribute("user") == null) {
+            return;
         }
-        return "1";
+
+        User sessionUser = (User) session.getAttribute("user");
+        Statistic statistic = sessionUser.getStatistic();
+        statistic.setGamesPlayed(statistic.getGamesPlayed() + 1);
+        sessionUser.setStatistic(statistic);
+        userService.update(sessionUser);
+
+        Long gamesPlayed = (Long) session.getAttribute("gamesPlayed");
+        if (gamesPlayed == null) {
+            Collection<User> users = userService.getAll();
+            gamesPlayed = 1L;
+            for (User user : users) {
+                gamesPlayed += user.getStatistic().getGamesPlayed();
+            }
+            session.setAttribute("gamesPlayed", gamesPlayed);
+        } else {
+            session.setAttribute("gamesPlayed", ++gamesPlayed);
+        }
     }
+
+    private void setVictory(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("user") == null) {
+            return;
+        }
+
+        User sessionUser = (User) session.getAttribute("user");
+        Statistic statistic = sessionUser.getStatistic();
+        statistic.setGamesWon(statistic.getGamesWon() + 1);
+        sessionUser.setStatistic(statistic);
+        userService.update(sessionUser);
+
+        Long gamesWon = (Long) session.getAttribute("gamesWon");
+        if (gamesWon == null) {
+            Collection<User> users = userService.getAll();
+            gamesWon = 1L;
+            for (User user : users) {
+                gamesWon += user.getStatistic().getGamesWon();
+            }
+            session.setAttribute("gamesWon", gamesWon);
+        } else {
+            session.setAttribute("gamesWon", ++gamesWon);
+        }
+    }
+
     private void setQuestName(HttpServletRequest request) {
-            String name = request.getParameter("name");
-            HttpSession session = request.getSession();
-            session.setAttribute("questName", name);
+        String name = request.getParameter("name");
+        HttpSession session = request.getSession();
+        session.setAttribute("questName", name);
     }
 
     private String getReason(HttpServletRequest request) {
