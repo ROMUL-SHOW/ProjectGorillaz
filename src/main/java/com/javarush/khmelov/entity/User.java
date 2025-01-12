faf3d2ba806c60a1734d0df55ac1bcaa47a0e5ba
shishkin <Craz9.Bunn9@yandex.ru>
2025-01-12 10:51:34 +0300

    feat: add statistic

--- src/main/java/com/javarush/khmelov/entity/User.java
+++ src/main/java/com/javarush/khmelov/entity/User.java
@@ -19,6 +19,16 @@ public class User {
 
     private Role role;
 
+    private Statistic statistic;
+
+    public User(Long id, String login, String password, Role role) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+        this.statistic = new Statistic(0L, 0L);
+    }
+
     public String getImage() { //TODO move to DTO
         return "image-" + id;
     }
